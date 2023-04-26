4fdf464454ebb9b6b62fe7481e3e06d71654d134
Lou Andria <lou@hei.school>
2023-04-26 18:35:27 +0200

    test: retries

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -36,7 +36,64 @@ def test_ping(apigw_ping_event):
     assert response["body"]  == "cG9uZw==" # "pong", for non-base64 fluent folks
 
 @responses.activate
-def test_ping_but_ping_is_unknown(apigw_ping_event):
+def test_ping_is_unknown(apigw_ping_event):
+    response = app.lambda_handler(apigw_ping_event, "")
+
+    assert response["statusCode"] == 500
+    assert "GiveUpRetryError" in response["body"]
+
+@responses.activate
+def test_ping_is_eventually_known(apigw_ping_event):
+    # 1 try
+    responses.add(
+        responses.GET,
+        "http://private-api-preprod.bpartners.app/ping",
+        status=429)
+    # 3 retries
+    responses.add(
+        responses.GET,
+        "http://private-api-preprod.bpartners.app/ping",
+        status=429)
+    responses.add(
+        responses.GET,
+        "http://private-api-preprod.bpartners.app/ping",
+        status=429)
+    responses.add(
+        responses.GET,
+        "http://private-api-preprod.bpartners.app/ping",
+        status=200)
+
+    response = app.lambda_handler(apigw_ping_event, "")
+
+    assert response["statusCode"] == 200
+
+@responses.activate
+def test_ping_is_known_too_late(apigw_ping_event):
+    # 1 try
+    responses.add(
+        responses.GET,
+        "http://private-api-preprod.bpartners.app/ping",
+        status=429)
+    # 3 retries
+    responses.add(
+        responses.GET,
+        "http://private-api-preprod.bpartners.app/ping",
+        status=429)
+    responses.add(
+        responses.GET,
+        "http://private-api-preprod.bpartners.app/ping",
+        status=429)
+    responses.add(
+        responses.GET,
+        "http://private-api-preprod.bpartners.app/ping",
+        status=429)
+    # 4-th retry that is never done...
+    responses.add(
+        responses.GET,
+        "http://private-api-preprod.bpartners.app/ping",
+        body="pong",
+        status=200)
+
     response = app.lambda_handler(apigw_ping_event, "")
 
     assert response["statusCode"] == 500
