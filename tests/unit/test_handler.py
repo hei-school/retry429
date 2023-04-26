82ee3649f6802fa96344ad96e27a1e524c7ce229
Lou Andria <lou@hei.school>
2023-04-26 18:13:06 +0200

    test: post with non b64 payload

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -1,7 +1,9 @@
 import pytest
-import responses
-from retry_429 import app
 import json
+import responses
+from responses import matchers
+
+from retry_429 import app
 
 def read_filepath_content(filepath):
     with open(filepath, "r") as json_file:
@@ -10,6 +12,10 @@ def read_filepath_content(filepath):
 @pytest.fixture()
 def apigw_ping_event():
     return read_filepath_content("./events/ping.json")
+
+@pytest.fixture()
+def apigw_post_non_b64_event():
+    return read_filepath_content("./events/post_non_b64.json")
 
 @responses.activate
 def test_ping(apigw_ping_event):
@@ -24,3 +30,17 @@ def test_ping(apigw_ping_event):
     assert response["statusCode"] == 200
     assert response["isBase64Encoded"]  == True
     assert response["body"]  == "cG9uZw==" # "pong", for non-base64 fluent folks
+
+@responses.activate
+def test_post_non_b64(apigw_post_non_b64_event):
+    responses.add(
+        responses.POST,
+        "http://private-api-preprod.bpartners.app/thepath",
+        match=[
+            matchers.json_params_matcher({ "foo": "bar €" })
+        ],
+        status=200)
+
+    response = app.lambda_handler(apigw_post_non_b64_event, "")
+
+    assert response["statusCode"] == 200
